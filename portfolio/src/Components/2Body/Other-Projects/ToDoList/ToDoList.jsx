9bf2aa63a6ef2cde66abfb9ce76efec57ac25dd0
Jordy Rocacher <jmr4277@orange.fr>
2023-08-16 22:18:08 +0200

    little content, localStorage in progress

--- portfolio/src/Components/2Body/Other-Projects/ToDoList/ToDoList.jsx
+++ portfolio/src/Components/2Body/Other-Projects/ToDoList/ToDoList.jsx
@@ -1,41 +1,81 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaCheck, FaTrash } from "react-icons/fa";
 
 export const ToDoList = () => {
   //function input
+  let todos = [];
+
+  if (localStorage.getItem("todos") === null) {
+    return (todos = []);
+  } else {
+    todos.forEach((todo) => {
+      createElementTodo(todo);
+    });
+  }
+  console.log(localStorage.getItem("todos").toString);
+
+  function inputCheck() {
+    let valueInput = document.querySelector(".input-todo")?.value;
+    //let value = valueInput;
+    createElementTodo(valueInput);
+    addStorage(valueInput);
+  }
+
+  function createElementTodo(value) {
+    // create element
+    let sectionTodo = document.querySelector(".section-to-do");
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    // btns todo
+    let trash = document.createElement("span");
+    let modif = document.createElement("span");
+    trash.classList.add("trash-to-do");
+    modif.classList.add("modif-to-do");
+    div.classList.add("to-do");
+
+    p.innerHTML = value;
+    div.append(p);
+    div.append(modif);
+    div.append(trash);
+
+    sectionTodo.append(div);
+    createTrash(trash);
+    createModif(modif);
+  }
 
   function inputTrash(clas) {
     document.querySelector(clas).value = "";
   }
+  // function set and get localeStorage
+  function addStorage(todo) {
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
 
-  function inputCheck() {
-    // create element
-    let valueInput = document.querySelector(".input-todo")?.value;
-    let value = valueInput;
-    let sectionTodo = document.querySelector(".section-to-do");
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-    let trash = document.createElement("span");
-    let modif = document.createElement("span");
-    // config element
-    div.classList.add("to-do");
-    p.innerHTML = value;
-    trash.classList.add("trash-to-do");
-    modif.classList.add("modif-to-do");
-    // add element in parent
-    div.append(p);
-    div.append(modif);
-    div.append(trash);
-    sectionTodo.append(div);
-    // add event todo
-    trash.addEventListener("click", () => {
-      div.remove();
+  /*function getStorage(element) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.forEach((todo) => {
+      element.innerHTML = todo;
     });
-    modif.addEventListener("click", (e) => {
+  }*/
+
+  // function trash and modif
+  function createModif(element) {
+    element.addEventListener("click", (e) => {
       let targetDiv = e.target.parentNode;
       let inputModif = document.createElement("input");
       inputModif.classList.add("modif-input");
-      if (targetDiv.childNodes[0] == document.querySelector(".modif-input")) {
+      if (targetDiv.childNodes[0] === document.querySelector(".modif-input")) {
         // add p and remove input
         let pTodo = document.createElement("p");
         pTodo.innerHTML = document.querySelector(".modif-input")?.value;
@@ -44,12 +84,19 @@ export const ToDoList = () => {
       } else {
         // add input and remove p
         inputModif.value = targetDiv.textContent;
-        console.log(targetDiv.removeChild(targetDiv.childNodes[0]));
+        targetDiv.removeChild(targetDiv.childNodes[0]);
         targetDiv.prepend(inputModif);
       }
     });
   }
 
+  function createTrash(element) {
+    element.addEventListener("click", () => {
+      element.parentNode.remove();
+    });
+  }
+
+  //localStorage.clear();
   return (
     <main className="to-do-list">
       <h1>TO DO LIST</h1>
